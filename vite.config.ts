4c466f0bb4457748e7c946acf9ea33082b7d64b2
BrandonML <brandon@movinglabor.com>
2025-10-09 20:03:17 -0700

    added base path to fix gh page issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/google-maps-timeline-converter/',
   build: {
     outDir: 'docs', // instead of 'dist'
   },
